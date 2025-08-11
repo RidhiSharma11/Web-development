e378ebe4f39aed3081f21c6ba695a24e8d1354d9
Ridhi Sharma <theridhiisharmaa@gmail.com>
2025-08-11 20:06:39 +0530

    added Js

--- Mini Projects JS/Simonsays.js
+++ Mini Projects JS/Simonsays.js
@@ -5,6 +5,8 @@ let btns=["pink","yellow","green","purple"];
 let started=false;
 let level=0;
 
+
+let h2=document.querySelector("h2");
 document.addEventListener("keypress",function(){
     if(started==false){
         console.log("Game is started");
@@ -14,7 +16,7 @@ document.addEventListener("keypress",function(){
     }
 });
 
-function btnflash(btn){
+function gameflash(btn){
     btn.classList.add("flash");
     setTimeout(function(){
         btn.classList.remove("flash");
@@ -22,12 +24,33 @@ function btnflash(btn){
 }
 
 
+function userflash(btn){
+    btn.classList.add("userflash");
+    setTimeout(function(){
+        btn.classList.remove("userflash");
+    },250);
+}
+
 function levelUp(){
     level++;
-    let h2=document.querySelector("h2");
     h2.innerText=`Level is ${level}`;
     let randIdx=Math.floor(Math.random()*3); // WHY??
     let randColor=btns[randIdx];
     let randBtn=document.querySelector(`.${randColor}`);
-    btnflash(randBtn);
+    console.log(randIdx);
+    console.log(randColor);
+    console.log(randBtn);
+    gameflash(randBtn);
+}
+
+function btnPress(){
+    console.log(this);
+    let btn=this;
+    userflash(btn);
+}
+
+
+let allBtns=document.querySelectorAll(".btn");
+for(btn of allBtns){
+    btn.addEventListener("click",btnPress);
 }
