7af228e411ca40769a2b74f60cab41b2faf2a875
Ridhi Sharma <theridhiisharmaa@gmail.com>
2025-09-18 10:23:27 +0530

    added

--- Backend/EJSDir/index.js
+++ Backend/EJSDir/index.js
@@ -8,11 +8,32 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
 
 app.get("/",(req,res)=>{
-    res.render("home.js");
+    res.render("home.ejs");
 });
 
 app.get("/hello",(req,res)=>{
     res.send("hello");
+});
+
+// app.get("/ig/:username",(req,res)=>{
+//     const followers=["Adam","Bob","Steve","Jam"];
+//     let {username}=req.params;
+//     res.render("instagram.ejs",{username,followers});
+// });
+
+app.get("/ig/:username",(req,res)=>{
+    let {username}=req.params;
+    const instaData=require("./data.json");
+    const data=instaData[username];
+    console.log(data);
+    res.render("instagram1.ejs",{data});
+});
+
+
+
+app.get("/dice",(req,res)=>{
+    let diceVal=Math.floor(Math.random()*6)+1;
+    res.render("rolldice.ejs",{diceVal});
 });
 
 app.listen(port,()=>{
